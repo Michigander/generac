3661e414380f6e46c0c45298396c5251926e00a5
Michigander <gordon.m.finnie.iii@gmail.com>
2021-07-10 17:28:14 -0400

    Allows merged stream members to complete

--- src/create-merged-stream.ts
+++ src/create-merged-stream.ts
@@ -1,7 +1,10 @@
+import { delay } from "./utils";
+
 export interface MetadataWrapped<T> {
   meta: {
     index: number;
     stream: AsyncGenerator<T>;
+    done: boolean;
   };
   value: T;
 }
@@ -10,8 +13,8 @@ async function takeLap<T>(
   stream: AsyncGenerator<T>,
   index: number
 ): Promise<MetadataWrapped<T>> {
-  const { done: _done, value } = await stream.next();
-  return { value, meta: { index, stream } };
+  const { done, value } = await stream.next();
+  return { value, meta: { index, stream, done: done ?? false } };
 }
 
 export async function* createMergedStream<T>(
@@ -19,7 +22,13 @@ export async function* createMergedStream<T>(
 ): AsyncGenerator<MetadataWrapped<T>> {
   const runners = streams.map(takeLap);
   while (true) {
-    const winner = await Promise.race(runners);
+    const winner = await Promise.race(runners.filter(Boolean));
+    if (winner.meta.done) {
+      //@ts-ignore
+      runners[winner.meta.index] = undefined;
+      continue;
+    }
+
     runners[winner.meta.index] = takeLap(winner.meta.stream, winner.meta.index);
     yield winner;
   }
